9a8622cc4ebc354312b4eaaec6ff4ca9afa053a1
Ethan Penn <empenn54@gmail.com>
2018-08-26 08:30:33 -0500

    Changing way text is added.

--- script.js
+++ script.js
@@ -14,8 +14,6 @@ $(document).ready(()=>{
 	
 	const spanLast = span();
 	spanLast.attr("id", "CodeEdit");
-	spanLast.addClass("codeText");
-	spanLast.text($("#Code").text());
 	$("#Code").empty();
 	$("#Code").append(spanLast);
 	
@@ -52,10 +50,12 @@ $(document).ready(()=>{
 		if(control && key === 82) location.reload();
 		switch(key){
 			case 8: removeChar(); break;// Case Backspace
+			case 9: appendChar("\t"); break;// Case Tab
+			case 13: appendChar('\n'); break;// Case Space
 			case 32: appendChar(' '); break;// Case Space
 			case 65:case 66:case 67:case 68:case 69:case 70:case 71:case 72:case 73:
-			case 74:case 75:case 76:case 77:case 78:case 79:case 80:case 81:case 81:
-			case 81:case 83:case 84:case 85:case 86:case 87:case 88:case 89:case 90:
+			case 74:case 75:case 76:case 77:case 78:case 79:case 80:case 81:case 82:
+			case 83:case 84:case 85:case 86:case 87:case 88:case 89:case 90:
 				// Case Alphabet
 				if(!shift) appendChar(baseAlphaChars[key - 65]);
 				else appendChar(shiftAlphaChars[key - 65]);
@@ -66,11 +66,14 @@ $(document).ready(()=>{
 	}
 	
 	function removeChar(){
-		$("#CodeEdit").text($("#CodeEdit").text().slice(0, -1));
+		//$("#CodeEdit").text($("#CodeEdit").text().slice(0, -1));
+		$("#CodeEdit").last().remove();
 	}
 	
 	function appendChar(c){
-		$("#CodeEdit").text($("#CodeEdit").text() + c);
+		const newElem = span().text(c);
+		$("#CodeEdit").append(newElem);
+		//$("#CodeEdit").text($("#CodeEdit").text() + c);
 	}
 	
 	function between(numb, min, max){
